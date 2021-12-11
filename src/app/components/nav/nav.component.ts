3187647154d919109afe329dd9b4617dfe4863bd
socratescosta09 <socratesaoc@gmail.com>
2021-12-11 10:49:24 -0300

    HTML e CSS para componente CHAMADO-LIST

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
 
   ngOnInit(): void {
    // this.router.navigate(['home']),
-    this.router.navigate(['tecnicos'])
+    this.router.navigate(['chamados'])
   }
   
   logout() {
