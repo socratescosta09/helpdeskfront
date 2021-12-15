1b721a128f963f0d151687463f56d593945aaef4
socratescosta09 <socratesaoc@gmail.com>
2021-12-15 12:05:17 -0300

    Ajustes no CSS

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -16,14 +16,13 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-   // this.router.navigate(['home']),
-    this.router.navigate(['chamados/read/1'])
+   this.router.navigate(['home'])
   }
   
   logout() {
     this.router.navigate(['login'])
     this.authService.logout();
-    this.toast.info('Logout realizado com sucesso', 'Logout');
+    this.toast.info('Logout realizado com sucesso', 'Logout')
   }
   
 }
